517672520871ce3ad35bd0fc2ffdfd28e5d7c7e8
nur-e <54142452+nur-e@users.noreply.github.com>
2021-12-22 16:25:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Our First StreamLit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''  This is our first Web App. 
+Enjoy it! 
+''')
